b2aee93c4b2211b9143f5d70b647402cc7162d18
youngmoon525 <youngmoon525@naver.com>
2022-12-07 16:04:46 +0900

    DB연결테스트-JUnit

--- spring02/src/test/java/co/kr/web02/DBConnTest.java
+++ spring02/src/test/java/co/kr/web02/DBConnTest.java
@@ -1,12 +1,42 @@
 package co.kr.web02;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
 
+import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration( "file:src/main/resources/db/conn.xml")
 public class DBConnTest {
 	
-	public void connect() throws Exception {
+	@Autowired private DataSource ds;
+	@Autowired private SqlSession sql;
+	
+	@Test
+	public void query_test() {
+		Date today = sql.selectOne("test.today");
+		System.out.println( "오늘: " + today );
+	}
+	
+	
+	@Test
+	public void connect() throws SQLException {
 		Connection conn = null;
-		
+		try {
+			conn = ds.getConnection();
+			System.out.println("DB Connection 성공: " + conn);
+		}catch(Exception e) {
+			System.out.println("DB Connection 실패");
+		}finally {
+			conn.close();
+		}
 	}
 }
