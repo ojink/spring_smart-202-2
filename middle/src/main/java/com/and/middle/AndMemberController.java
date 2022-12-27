068b7e7f95ca7e660b26fe972cea3e370cc2a36c
youngmoon525 <youngmoon525@naver.com>
2022-12-27 11:17:56 +0900

    안드로이드 미들웨어

--- middle/src/main/java/com/and/middle/AndMemberController.java
+++ middle/src/main/java/com/and/middle/AndMemberController.java
@@ -2,14 +2,19 @@ package com.and.middle;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
 
 import com.google.gson.Gson;
 
+import common.CommonService;
 import member.MemberVO;
 
 
@@ -32,7 +37,23 @@ public class AndMemberController {
 		System.out.println(email);
 		return new Gson().toJson("");
 	}
-	
+	@Autowired CommonService common;
+	@RequestMapping(value = "/join.me" , produces = "text/html;charset=utf-8")
+	public String join(String param , HttpServletRequest req) {
+		MemberVO vo = new Gson().fromJson(param, MemberVO.class);
+		MultipartRequest mReq = (MultipartRequest) req;
+		MultipartFile file = mReq.getFile("file");
+		String imgPath = null;
+		if(file !=null) {
+			System.out.println(file.getOriginalFilename());
+			System.out.println(file.getName());
+			imgPath = common.fileUpload("and", file, req);
+			vo.setFile_path(imgPath);
+			System.out.println(imgPath);	
+		}
+		int result = sql.insert("me.join" , vo );
+		return new Gson().toJson(result + "");
+	}
 	
 	
 }
